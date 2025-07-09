c615290654da82a7a6fe81615ef160f17f86e871
Dhegnaas <abdirahman460930@gmail.com>
2025-07-09 16:57:27 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+import taskform from './components/taskform.vue';
+import type {task} from './types' ;
+
+
+const message = ref("Tasks app");
+const Tasks = ref<task[]>([]);
+
+function addtask(newtask: string){
+  Tasks.value.push({
+    id: crypto.randomUUID(),
+    title: newtask,
+    done: false,
+  });
+}
+</script>
+
+<template>
+  <main>
+    <h1>{{ message }}</h1>
+    <h3>there are{{ Tasks.length }} tasks. </h3>
+    <taskform @addtask="addtask" />
+  </main>
+</template>
+
+<style>
+main{
+  max-width: 800px;
+  margin: 1rem auto;
+}
+ 
+</style>
---
+++ src/components/taskform.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const emit = defineEmits<{
+   addtask: [newtask: string]  
+}>();
+
+const newtask = ref("");
+
+function formsubmitted(){
+    emit("addtask",newtask.value);
+}   
+
+</script>
+<template>
+        <form @submit.prevent="formsubmitted">
+      <label>
+        New Task
+        <input v-model="newtask" name="new task">
+      </label>
+      <div class="button-container">
+        <button>Add</button>
+      </div>
+    </form>
+</template>
